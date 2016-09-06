face06df81774dedcbc1f4418995f95dd8366221
Matt Bartling <Matt Bartling>
2016-09-06 11:29:02 -0500

    adding more stuff

--- public/chat.js
+++ public/chat.js
@@ -5,13 +5,23 @@ window.onload = function() {
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
-
+	var name = document.getElementById("name");
+	
+	$(document).ready(function() {
+		$("#field").keyup(function(e) {
+			if(e.keyCode == 13) {
+				sendMessage();
+			}
+		});
+	});
     socket.on('message', function (data) {
         if(data.message) {
-            messages.push(data.message);
+            messages.push(data);
             var html = '';
             for(var i=0; i<messages.length; i++) {
-                html += messages[i] + '<br />';
+				console.log(messages[i]);
+				html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ":  </b>";
+                html += messages[i].message + '<br />';
             }
             content.innerHTML = html;
         } else {
@@ -19,9 +29,15 @@ window.onload = function() {
         }
     });
 
-    sendButton.onclick = function() {
-        var text = field.value;
-        socket.emit('send', { message: text });
+    sendButton.onclick = sendMessage = function() {
+		console.log(name.value)
+		if (name.value == "") {
+			alert("Please type your name!")
+		} else {
+			var text = field.value;
+			socket.emit('send', { message: text, username: name.value });
+			field.value = "";
+		}
     };
 
 }
